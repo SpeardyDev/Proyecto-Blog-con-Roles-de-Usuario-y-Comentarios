67ab0c41c287c433e65bbaab1dfff6539175597e
Nicolas Mahecha <nicolasmahechamartinez@gmail.com>
2025-07-31 15:13:19 -0500

    31/07/2025 - Se crea el archivo bd.go y se actualiza a los datos de la base de datos, aún no se completa la conexión en proceso de solucionarse, se importa una dependencia que es la de MySQL para poder hacer la conexión.

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -5,5 +5,4 @@ import "fmt"
 func main() {
 	fmt.Println("Blog backend service running...")
 	fmt.Println("Blog backend service stop...")
-	fmt.Println("Pinche wey")
 }
--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"blog-backend/internal/config"
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	db, err := config.ConnectDB()
+	if err != nil {
+		log.Fatalf("Error conectando a la base de datos: %v", err)
+	}
+	defer db.Close()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
